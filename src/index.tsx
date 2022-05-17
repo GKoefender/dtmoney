bfc5cfc23482d42159e2bb1e5caa57098d285111
GKoefender <gustavokoefender1999@hotmail.com>
2022-05-17 13:36:23 -0300

    axios e configuração do mirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
+import { createServer } from 'miragejs'
+
+createServer({
+  routes () {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: '1',
+          title: 'Desenvolvimento site',
+          price: '1000',
+          category: 'Renda',
+          type: 'deposit',
+          date: '17/05/2022'
+        },
+        {
+          id: '2',
+          title: 'Alimentacao',
+          price: '500',
+          category: 'Renda',
+          type: 'restaurante',
+          date: '17/05/2022'
+        }
+      ]
+    })
+  }
+})
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
